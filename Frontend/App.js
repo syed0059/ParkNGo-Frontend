88d44ca079bcd7774c2655550b0a1afed70474c1
kenneth <tanwjkenneth@gmail.com>
2023-10-16 22:19:00 +0800

    Added Frontend

---
+++ Frontend/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});

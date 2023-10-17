0e4b53a90c22a80b913c43795509f65ef161b1eb
syed0059 <79743238+syed0059@users.noreply.github.com>
2023-10-17 22:42:13 +0800

    comments

--- Backend/CarparkInterface/carparkInterface.js
+++ Backend/CarparkInterface/carparkInterface.js
@@ -5,6 +5,12 @@ const mongoClient = db.getDB();
 
 const collection = mongoClient.collection('carparks')
 
+/**
+ *  The base URI for this interface is /carpark, so whatever endpoints we write 
+ *  here will come after /carpark, eg. localhost:3000/carpark/all - to get all carparks
+ */
+
+
 router.get('/', async (req, res) => {
     collection.insertOne({'a': 1});
     res.send(await collection.find().toArray());

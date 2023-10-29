2e3bafd8574cdb2adb93d6236b9b6eebf683f0e0
hiimstevenzhu <123613244+hiimstevenzhu@users.noreply.github.com>
2023-10-30 02:05:23 +0800

    prices update

---
+++ src/components/CarparkInfo/Prices/HDBPrices.jsx
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function HDBPrices() {
+  const carPrices =
+    "7.00am to 10:30pm: $0.60 / 30min \n\n10.30pm to 7.00am: $0.60 / 30min \n\nParking from 10.30pm to 7.00am capped at $5\n";
+  const motorcyclePrices =
+    "7.00am to 10:30pm: $0.20 / 30min \n\n10.30pm to 7.00am: $0.20 / 30min \n\nSession is capped at $0.65 per lot\n";
+  const terms = "Per minute charging";
+  return (
+    <View>
+      <Text style={curstyles.header}>Car:</Text>
+      <Text>{carPrices}</Text>
+      <Text style={curstyles.header}>Motorcycle:</Text>
+      <Text>{motorcyclePrices}</Text>
+      <Text style={curstyles.header}>{terms}</Text>
+    </View>
+  );
+}
+
+const curstyles = StyleSheet.create({
+  header: {
+    fontWeight: "bold",
+    fontSize: "15",
+  },
+});
--- src/components/CarparkInfo/Prices/Prices.jsx
+++ src/components/CarparkInfo/Prices/Prices.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Card, Text } from "react-native-paper";
 import { View, StyleSheet } from "react-native";
+import HDBPrices from "./HDBPrices";
 
 export default function Prices() {
+  var now = new Date();
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var day = days[now.getDay()];
   return (
     <View style={curStyles.container}>
       <Card>
-        <Card.Title title="Prices" />
+        <Card.Title title={day + "'s Prices"} />
         <Card.Content>
-          <Text>Test</Text>
+          <HDBPrices />
         </Card.Content>
       </Card>
     </View>

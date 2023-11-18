f310897c74d526fa6a28dda95f695c2cccb6fed2
hiimstevenzhu <123613244+hiimstevenzhu@users.noreply.github.com>
2023-11-19 02:12:55 +0800

    pins fix

--- src/components/MapView/MapInterface.js
+++ src/components/MapView/MapInterface.js
@@ -1,27 +1,30 @@
-import MapView, { Marker, PROVIDER_GOOGLE, Circle } from 'react-native-maps';
-import { StyleSheet, View, Button } from 'react-native';
-import { getFavourites, getCarparksByIdArray } from '../../carparkInterface/carparkInterface';
-import { useEffect, useState, useContext, useCallback } from 'react';
-import * as Location from 'expo-location';
-import { RadiusContext } from '../../searchManager/RadiusContext'
-import { MapCoordinates } from '../../mapViewManager/MapCoordinatesContext';
-import { MapSearchCoordinates } from '../../mapViewManager/MapSearchContext';
-import { MapCenterToPin } from '../../mapViewManager/MapCenterToPinContext';
+import MapView, { Marker, PROVIDER_GOOGLE, Circle } from "react-native-maps";
+import { StyleSheet, View, Button } from "react-native";
+import {
+  getFavourites,
+  getCarparksByIdArray,
+} from "../../carparkInterface/carparkInterface";
+import { useEffect, useState, useContext, useCallback } from "react";
+import * as Location from "expo-location";
+import { RadiusContext } from "../../searchManager/RadiusContext";
+import { MapCoordinates } from "../../mapViewManager/MapCoordinatesContext";
+import { MapSearchCoordinates } from "../../mapViewManager/MapSearchContext";
+import { MapCenterToPin } from "../../mapViewManager/MapCenterToPinContext";
 import FavouritesContext from "../../favouritesManager/FavouritesContext";
-import { calculateDistance } from '../../searchManager/CalculateDistance';
+import { calculateDistance } from "../../searchManager/CalculateDistance";
 import ActiveFavouritesContext from "../../favouritesManager/ActiveFavouritesContext";
-import { getCarparks } from '../../mapViewManager/MapManager';
-import _ from 'lodash'; 
+import { getCarparks } from "../../mapViewManager/MapManager";
+import _ from "lodash";
 
-export default function Map({ location, loading, carparks }){
-
+export default function Map({ location, loading, carparks }) {
+  console.log(location);
   const { isFavouritesActive } = useContext(ActiveFavouritesContext);
   const { favourites } = useContext(FavouritesContext);
   const [locationsOfInterest, setLocationsOfInterest] = useState([]);
   const { radius } = useContext(RadiusContext);
   const { mapCoordinates, setMapCoordinates } = useContext(MapCoordinates);
   const { mapSearchCoordinates } = useContext(MapSearchCoordinates);
-  const { mapCenterToPin, setMapCenterToPin} = useContext(MapCenterToPin);
+  const { mapCenterToPin, setMapCenterToPin } = useContext(MapCenterToPin);
 
   // To update list of carparks to be showns
   const addIn = async () => {
@@ -30,12 +33,12 @@ export default function Map({ location, loading, carparks }){
     if (isFavouritesActive) {
       const favouriteCarparks = await getFavourites();
       const carparksByFavs = await getCarparksByIdArray(favouriteCarparks);
-      data=getCarparks(carparksByFavs);
+      data = getCarparks(carparksByFavs);
     } else {
-      data=getCarparks(carparks);
+      data = getCarparks(carparks);
     }
 
-    setLocationsOfInterest(data)
+    setLocationsOfInterest(data);
   };
 
   // Drawing out the pins
@@ -52,68 +55,70 @@ export default function Map({ location, loading, carparks }){
       return (
         <Marker
           pinColor={color}
-          key={index}
+          key={`${item.location.latitude}${item.location.longitude}`}
           coordinate={item.location}
           title={item.title}
           description={item.description}
-          onPress={e => setMapCenterToPin(e.nativeEvent.coordinate)}
-        >
-        </Marker>
+          onPress={(e) => setMapCenterToPin(e.nativeEvent.coordinate)}
+        ></Marker>
       );
     });
   };
 
-  const centerToPin = async (coords) => { 
-    this.map.animateCamera({center:coords}, {duration:500})
+  const centerToPin = async (coords) => {
+    this.map.animateCamera({ center: coords }, { duration: 500 });
   };
 
   useEffect(() => {
-    centerToPin(mapCenterToPin)
-  }, [mapCenterToPin])
+    centerToPin(mapCenterToPin);
+  }, [mapCenterToPin]);
 
   // Update pins when carpark list is updated
   useEffect(() => {
     addIn();
-  }, [carparks])
+  }, [carparks]);
 
   // Updates map view when user searches in the search bar
   useEffect(() => {
-    if(mapSearchCoordinates.latitude!=undefined && mapSearchCoordinates.longitude!=undefined){
+    if (
+      mapSearchCoordinates.latitude != undefined &&
+      mapSearchCoordinates.longitude != undefined
+    ) {
       setMapCoordinates({
         latitude: mapSearchCoordinates.latitude,
-        longitude:mapSearchCoordinates.longitude,
+        longitude: mapSearchCoordinates.longitude,
         latitudeDelta: 0.008540807106718562,
         longitudeDelta: 0.008127428591251373,
-      })
+      });
     }
-  }, [mapSearchCoordinates])
+  }, [mapSearchCoordinates]);
 
   // Update map to show pins depending if user is in favourite menu or in other menu
   useEffect(() => {
-    if(!isFavouritesActive){
+    if (!isFavouritesActive) {
       fetchLocation();
-    }else{
+    } else {
       addIn();
     }
   }, [isFavouritesActive]);
 
   // Updates pin when favourites are added/removed
   useEffect(() => {
-    if(isFavouritesActive){
+    if (isFavouritesActive) {
       addIn();
     }
   }, [favourites]);
 
   // Get user location
-  const fetchLocation = async () => { 
+  const fetchLocation = async () => {
     try {
       let { status } = await Location.requestForegroundPermissionsAsync();
-      
-      if (status !== 'granted') {
-        console.error('Location permission not granted');
+
+      if (status !== "granted") {
+        console.error("Location permission not granted");
         return;
       }
-      
+
       let location = await Location.getCurrentPositionAsync({
         accuracy: Location.Accuracy.Balanced,
       });
@@ -123,7 +128,7 @@ export default function Map({ location, loading, carparks }){
         longitude: location.coords.longitude,
         latitudeDelta: 0.008540807106718562,
         longitudeDelta: 0.008127428591251373,
-      })
+      });
     } catch (error) {
       console.error(error);
     }
@@ -135,44 +140,57 @@ export default function Map({ location, loading, carparks }){
   }, []);
 
   // Update map coordinates when not in favourite menu
-  const debouncedOnRegionChange = useCallback(_.debounce((region) => {
-    if(calculateDistance(mapCoordinates.latitude, mapCoordinates.longitude, region.latitude, region.longitude) >= 1 && !isFavouritesActive){
-      setMapCoordinates({
-        latitude: region.latitude,
-        longitude: region.longitude,
-        latitudeDelta: region.latitudeDelta,
-        longitudeDelta: region.longitudeDelta,
-      });
-    }
-  }, 700));
+  const debouncedOnRegionChange = useCallback(
+    _.debounce((region) => {
+      if (
+        calculateDistance(
+          mapCoordinates.latitude,
+          mapCoordinates.longitude,
+          region.latitude,
+          region.longitude
+        ) >= 1 &&
+        !isFavouritesActive
+      ) {
+        setMapCoordinates({
+          latitude: region.latitude,
+          longitude: region.longitude,
+          latitudeDelta: region.latitudeDelta,
+          longitudeDelta: region.longitudeDelta,
+        });
+      }
+    }, 700)
+  );
 
   // Function is called when map is moved
   const onRegionChange = (region, gesture) => {
     // if (gesture.isGesture) {
-      debouncedOnRegionChange(region);
+    debouncedOnRegionChange(region);
     // }
-  }
+  };
 
   // Shows circle of search when not in favourite menu
   const showCircle = () => {
-    if(!isFavouritesActive){
-      return <Circle
-      center = {{
-        latitude: mapCoordinates.latitude,
-        longitude: mapCoordinates.longitude,
-      }} 
-      radius= {radius * 1000}
-      strokeColor='blue'
-      strokeWidth={2}/>
+    if (!isFavouritesActive) {
+      return (
+        <Circle
+          center={{
+            latitude: mapCoordinates.latitude,
+            longitude: mapCoordinates.longitude,
+          }}
+          radius={radius * 1000}
+          strokeColor="blue"
+          strokeWidth={2}
+        />
+      );
     }
   };
 
   return (
     <View style={styles.container}>
       <MapView
-      ref={ref=>{
-        this.map=ref;
-      }}
+        ref={(ref) => {
+          this.map = ref;
+        }}
         style={styles.map}
         provider={PROVIDER_GOOGLE}
         region={mapCoordinates}
@@ -184,11 +202,10 @@ export default function Map({ location, loading, carparks }){
       >
         {showLocationsOfInterest()}
         {showCircle()}
-
       </MapView>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
